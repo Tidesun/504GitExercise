981bb549efdab1a7f81615bc9ddd94674bb13213
Haoran Li <lihaoran9836@gmail.com>
2023-08-23 09:48:47 -0400

    add comments and docstring;make variable name descriptive

--- code.py
+++ code.py
@@ -1,16 +1,19 @@
-def function1(a):
-    b = dict()
-    for c in a:
-        if c not in b:
-            b[c] = 1
+def get_base_count_from_sequence(sequence):
+    '''Get count of nucleotide bases from DNA/RNA sequence'''
+    base_count_dict = dict()
+    for base in sequence:
+        if base not in base_count_dict:
+            base_count_dict[base] = 1
         else:
-            b[c] += 1
-    return b
+            base_count_dict[base] += 1
+    return base_count_dict
 
-def function2(a):
+def print_base_freq_from_count(base_count_dict):
+    '''Print the frequence of nucleotide bases from the count'''
     print('freqs')
-    total = float(sum([a[b] for b in a.keys()]))
-    for b in a.keys():
-        print(b + ':' + str(a[b]/total))
-
-function2(function1('ATCTGACGCGCGCCGC'))
+    total = float(sum([base_count_dict[base] for base in base_count_dict.keys()]))
+    for base in base_count_dict.keys():
+        print(base + ':' + str(base_count_dict[base]/total))
+sequence = 'ATCTGACGCGCGCCGC'
+base_count_dict = get_base_count_from_sequence(sequence)
+print_base_freq_from_count(base_count_dict)
